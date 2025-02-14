251f65811b5898627a5b7e9767ceb571056ceccd
gennadiy.kondratev <gennadiy.kondratev@vk.team>
2025-02-14 20:43:54 +0300

    сделан фронтэнд восстановления пароля

---
+++ web/auth_app/static/auth_app/js/reset-password-sent.js
@@ -0,0 +1,37 @@
+$(function () {
+  $('#recoveryForm').submit(submit);
+});
+
+function submit(e) {
+  e.preventDefault();
+
+  const form = $(this);
+  const fromData = new FormData(this);
+  const urlSearchParams = new URLSearchParams(window.location.search);
+
+  if (fromData.get('password_1') !== fromData.get('password_2')) {
+    alert('Error! Password and password confirmation are not equal.');
+
+    return;
+  }
+
+  $.ajax({
+    url: form.attr('action'),
+    type: 'POST',
+    dataType: 'json',
+    data: `${form.serialize()}&${urlSearchParams.toString()}`,
+    success: function (data) {
+      alert('Success! Password was updated. You can login with new credentials.');
+    },
+    error: function (error) {
+      let text = 'Server error.';
+
+      switch (error.status) {
+        case 400:
+          text = 'Some form\'s fields are invalid.';
+      }
+
+      alert(`Error! ${text}`);
+    }
+  })
+}
